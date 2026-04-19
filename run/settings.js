7b1be38f09575aa6835e02169fb1ab030f265645
Eric G <blueldr.eg@gmail.com>
2026-04-19 14:05:56 -0400

    Added getTickLabel function for custom tick labels

--- run/settings.js
+++ run/settings.js
@@ -16,11 +16,11 @@ export const settings = {
     define: { 'process.env.NODE_ENV': `'production'` }, // dev, production
     minify: true,
     target: ['es6'],
-    outfile: './dist/mz-react-round-slider.min.js',
+    outfile: './dist/blueldr-react-round-slider.min.js',
     plugins: [],
     banner: {
         js: `/* 
-MZ React Round Slider v${ version }
+blueldr React Round Slider v${ version }
 https://github.com/mzusin/react-round-slider
 MIT License      
 Copyright (c) 2023-present, Miriam Zusin          
